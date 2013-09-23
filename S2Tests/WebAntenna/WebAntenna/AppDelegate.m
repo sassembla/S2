3a8380934a2a2f31ca826ede38a877912879a3d2
Toru <toru.inoue@kissaki.tv>
2013-09-23 13:15:59 +0900

    Initial Commit

---
+++ S2Tests/WebAntenna/WebAntenna/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  WebAntenna
+//
+//  Created by sassembla on 2013/09/23.
+//  Copyright (c) 2013年 sassembla. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ S2Tests/WebAntenna/WebAntenna/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  WebAntenna
+//
+//  Created by sassembla on 2013/09/23.
+//  Copyright (c) 2013年 sassembla. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
